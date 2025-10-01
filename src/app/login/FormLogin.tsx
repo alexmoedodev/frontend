1a3cb85f29bd539a15084869c9ffe098472d7ecf
Alexmoedo <117864509+LAsolucoes@users.noreply.github.com>
2025-09-30 23:00:03 -0300

    FormLogin
    Formulario
    - typagem dos inputs
    - Pegar valores dos inputs
    - Comentarios

--- src/app/login/FormLogin.tsx
+++ src/app/login/FormLogin.tsx
@@ -1,17 +1,40 @@
+"use client"
+//📦 React
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+
+//🧩 Components
+import { Button } from "@/components/ui/Button/Button";
+import Input from "@/components/ui/Input/Input";
+
+//🎨 Styles
+import styles from "./formLogin.module.css";
 import ImageLoginBg from "../../../public/login.png";
 import LogoTipo from "../../../public/Logotipo.png";
-import styles from "./formLogin.module.css";
-import Input from "@/components/ui/Input/Input";
-import { Button } from "@/components/ui/Button/Button";
+
+//📋 typagem
+type FormLoginProps = {
+  email: string
+  password: string
+  remmember: boolean
+}
+
+
 export function FormLogin() {
+  const [user, setUser] = useState<FormLoginProps>({
+    email: "",
+    password: "",
+    remmember: false
+  })
+  console.log("Checkbox:", user.remmember)
+
   return (
     <>
       <div className={styles.login__container}>
         <div className={styles.login__card}>
           <div className={styles.login__image__bg}>
-            <Image src={ImageLoginBg} alt="" />
+            <Image src={ImageLoginBg} alt="Imagem de background do card" priority />
           </div>
 
           <form action="" className={styles.form__login}>
@@ -20,29 +43,39 @@ export function FormLogin() {
                 <Image
                   src={LogoTipo}
                   alt="Logo tipo da empresa desenvolvedora do sistema"
+                  priority
                 />
                 <p>Preencha suas credenciais para realizar login.</p>
               </div>
               <div className={styles.input__form}>
                 <Input
                   sizeStyles="w-full"
-                  title="Preencha seu email cadastrado."
+                  title="Preencha email cadastrado."
                   placeholder="email@email.com"
                   type="email"
+                  name="email"
+                  value={user.email}
+                  onChange={(e) => setUser((prev) => ({ ...prev, email: e.target.value }))}
+
                 />
                 <Input
                   sizeStyles="w-full"
                   title="Preencha sua senha."
                   placeholder="******"
                   type="password"
+                  name="password"
+                  value={user.password}
+                  onChange={(e) => setUser((prev) => ({ ...prev, password: e.target.value }))}
                 />
                 <div className={styles.remember__forgot_password}>
                   <div className={styles.remember}>
                     <input
                       type="checkbox"
-                      name=""
+                      name="remember"
                       id="remember"
                       title="Click para relembrar as credenciais"
+                      checked={user.remmember}
+                      onChange={(e) => setUser((prev)=> ({...prev , remmember: e.target.checked}))}
                     />
                     <label
                       htmlFor="remember"
@@ -60,7 +93,7 @@ export function FormLogin() {
                     </Link>
                   </div>
                 </div>
-                <Button variant="violet" size="w-full" title="Acessar ao Painel">
+                <Button type="submit" variant="violet" size="w-full" title="Acessar ao Painel">
                   Acessar
                 </Button>
               </div>
