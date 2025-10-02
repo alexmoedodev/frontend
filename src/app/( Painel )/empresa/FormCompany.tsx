23f2f183825f55b5ba710cc4a854ce1bb0aa3e1e
Alex Moedo <alexmoedo@gmail.com>
2025-10-02 10:35:29 -0300

    Update FormCompany.tsx

--- src/app/( Painel )/empresa/FormCompany.tsx
+++ src/app/( Painel )/empresa/FormCompany.tsx
@@ -1,4 +1,4 @@
-import  Layuot  from "../components/layout"
+import Layuot from "../components/layout"
 
 
 export function FormCompany(){
@@ -10,7 +10,6 @@ export function FormCompany(){
             <h1>Lorem, ipsum.</h1>
 
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum eius qui natus nulla saepe rem debitis ut. Veritatis laboriosam eveniet quidem. Pariatur incidunt quia hic aspernatur, aut debitis et error saepe omnis laudantium rem adipisci commodi placeat repellendus corrupti, iste nostrum rerum consequatur eos. Nostrum ad illum sapiente tenetur temporibus, aliquam provident eaque laudantium officiis. Rem quis, odio excepturi sint quaerat ad optio cum minima corrupti eaque facilis voluptatibus, cupiditate assumenda sunt facere beatae accusamus! Totam incidunt unde ipsam aperiam a sunt .</p>
-
         </div>
         
         
